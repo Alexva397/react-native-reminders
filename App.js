bd34ed7f914e28018a918baa39f536fceea62455
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-09-04 17:47:35 -0500

    created input field and handle functions for submission

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [reminder, setReminder] = useState('');
+
+  const [reminderList, setReminderList] = useState([]);
+
+  const reminderInput = (userReminder) => {
+    setReminder(userReminder);
+  }
+
+  const handleUserSubmit = () => {
+    console.log(reminder)
+
+    setReminderList([...reminderList, reminder]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.viewPort}>
+      <View style={styles.container}>
+        <TextInput 
+          placeholder='Add a Reminder' 
+          style={styles.input} 
+          onChangeText={reminderInput}
+          value={reminder}
+        />
+        <Button 
+          title='+ Add' 
+          style={styles.button}
+          onPress={handleUserSubmit}
+        />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  viewPort: {
+    padding: 30, 
+    marginTop: 30,
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row', 
+    justifyContent: 'space-around',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {
+    borderBottomColor: 'black', 
+    borderBottomWidth: 1,
+    width: '70%',
+  },
+  button: {
+    borderRadius: 5,
+    margin: 10,
   },
 });
